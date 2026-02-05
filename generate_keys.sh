8718705d07b3bda73e8b974c24a7f8de71cb7d78
boris <unclebob20@gmail.com>
2026-02-05 19:03:24 +0100

    Switch to ICO 56051026 for API_TOKEN and Keys

--- generate_keys.sh
+++ generate_keys.sh
@@ -14,14 +14,14 @@ echo "🔓 Generating Public Key (PEM)..."
 openssl rsa -in "$DIR/gov_fake_private.key" -pubout -out "$DIR/gov_fake_public.pem"
 
 echo "📜 Generating Self-Signed Certificate..."
-openssl req -new -x509 -key "$DIR/gov_fake_private.key" -out "$DIR/cert.pem" -days 3650 -subj "/CN=hostshield/O=HostShield/C=SK"
+openssl req -new -x509 -key "$DIR/gov_fake_private.key" -out "$DIR/cert.pem" -days 3650 -subj "/CN=56051026/O=HostShield/C=SK"
 
 echo "📦 Creating PKCS12 Keystore..."
 openssl pkcs12 -export \
     -in "$DIR/cert.pem" \
     -inkey "$DIR/gov_fake_private.key" \
     -out "$DIR/host_shield_test_prod.keystore" \
-    -name "hostshield" \
+    -name "56051026" \
     -passout pass:changeit
 
 # Cleanup intermediate cert
